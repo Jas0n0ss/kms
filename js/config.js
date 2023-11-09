9815dbd0b82b5d55067cabe0d41ae97f46711685
Jas0n0ss <88020021+Jas0n0ss@users.noreply.github.com>
2023-11-09 10:40:26 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,13 +2,18 @@ window.Config = {
   links: [
     {
       id: 0,
-      name: 'OSS',
-      url: 'https://oss.msft.vip'
+      name: 'GitHub',
+      url: 'https://github.com/Jas0n0ss/kms'
     },
     {
       id: 1,
-      name: 'MSDL',
+      name: 'ISO',
       url: 'https://oss.msft.vip/msdl'
+    },
+    {
+      id: 3,
+      name: 'KMS',
+      url: 'https://oss.msft.vip/2023/11/09/kms-server-for-windows-office/'
     }
   ],
 }
