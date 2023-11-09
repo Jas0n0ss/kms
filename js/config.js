cc338287b18c95f200f9bda3dcacee631bb0e5d3
Jason Hu(Jas0n0ss) <job.bo@outlook.com>
2023-11-09 22:05:17 +0800

    new update

--- js/config.js
+++ js/config.js
@@ -2,19 +2,13 @@ window.Config = {
   links: [
     {
       id: 0,
-      name: 'MSDL',
-      url: 'https://oss.msft.vip/msdl/'
-    },
-    {
-      id: 1,
-      name: 'BLOG',
+      name: 'OSS',
       url: 'https://oss.msft.vip'
     },
     {
       id: 1,
-      name: 'Github',
-      #url: 'https://github.com/ikxin/kms-tools'
-      url: 'https://github.com/Jas0n0ss/kmsrv'
+      name: 'MSDL',
+      url: 'https://oss.msft.vip/msdl'
     }
   ],
 }
