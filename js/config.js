8f8ce4b55e7a163d3246db0a4def6f43f2b92397
Jason Hu(Jas0n0ss) <job.bo@outlook.com>
2023-11-11 01:01:11 +0800

    new update

--- js/config.js
+++ js/config.js
@@ -2,36 +2,31 @@ window.Config = {
   links: [
     {
       id: 0,
-      name: 'GitHub',
-      url: 'https://github.com/Jas0n0ss/kms'
-    },
-    {
-      id: 1,
       name: 'ISO',
       url: 'https://oss.msft.vip/msdl'
     },
     {
-      id: 2,
-      name: 'KMS',
-      url: 'https://oss.msft.vip/2023/11/09/kms-server-for-windows-office/'
+      id: 1,
+      name: 'GitHub',
+      url: 'https://oss.msft.vip/msdl'
     },
-     {
-      id: 3,
+    {
+      id: 2,
       name: 'office',
       url: 'https://pesktop.com/en/windows/microsoft-office-202120192016-pro-plus'
     },
-     {
-      id: 4,
+    {
+      id: 3,
       name: 'AtlasOS',
       url: 'https://atlasos.net'
     },
-     {
-      id: 5,
+    {
+      id: 4,
       name: 'AtlasOS-10',
       url: 'https://pesktop.com/en/systems/windows-10-revios'
     },
     {
-      id: 6,
+      id: 5,
       name: 'AtlasOS-11',
       url: 'https://pesktop.com/en/systems/windows-11-revios-22h2'
     },
