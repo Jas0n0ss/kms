afa4d6c563f499e62a2e4f8250fb1e34dc0ea8f6
Jason Hu(Jas0n0ss) <job.bo@outlook.com>
2023-11-11 12:08:24 +0800

    new update

--- js/config.js
+++ js/config.js
@@ -2,25 +2,21 @@ window.Config = {
   links: [
     {
       id: 0,
-      name: 'ISO',
-    },
-    {
-      id: 0,
-      name: 'Win11|Win10',
+      name: 'MSDL',
       url: 'https://oss.msft.vip/msdl'
     },
     {
-      id: 0,
+      id: 1,
       name: 'Pesktop',
       url: 'https://pesktop.com/en/systems/page'
     },
     {
-      id: 0,
+      id: 2,
       name: 'Share',
       url: 'http/share.msft.vip/azure/iso/windows/'
     },
     {
-      id: 1,
+      id: 3,
       name: 'GitHub',
       url: 'https://github.com/Jas0n0ss/kms'
     },
